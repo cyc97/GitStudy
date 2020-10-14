47be15a80440b71cf69151d519e88b577a26c795
motivate <motivate@DESKTOP-S53S96S>
2020-10-14 09:23:09 +0800

    于嵩提交

--- GitStudy/src/main/Ys.java
+++ GitStudy/src/main/Ys.java
@@ -1,10 +1,19 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ys {
 void out(ArrayList arr) {
-		
+		Collections.sort(arr);
+		Collections.reverse(arr);
+		System.out.println("排序后结果：");
+		for(int i=0;i<arr.size();i++)
+		{
+			int temp = (int)arr.get(i);
+			System.out.print(temp+" ");
+		}
+		System.out.println("");
 	}
 
 }

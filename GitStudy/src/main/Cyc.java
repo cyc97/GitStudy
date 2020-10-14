5c5549e7692efef51017b409a337f574ea3b16ab
cyc97 <805756872@qq.com>
2020-10-14 09:54:51 +0800

    程业超提交

--- GitStudy/src/main/Cyc.java
+++ GitStudy/src/main/Cyc.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 
 public class Cyc {
 void out(ArrayList arr) {
-		
+	int sum=0;
+	for(int i=0;i<arr.size();i++){
+		sum+=(int)arr.get(i);
+	}
+	System.out.println("平均值为："+sum/arr.size());
 	}
 
 }

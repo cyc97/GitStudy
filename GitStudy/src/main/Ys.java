d70ed737b3954e1fd7884ea6f5ed2f079414f455
motivate <motivate@DESKTOP-S53S96S>
2020-10-14 09:34:50 +0800

    于嵩提交

--- GitStudy/src/main/Ys.java
+++ GitStudy/src/main/Ys.java
@@ -1,10 +1,19 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Ys {
 void out(ArrayList arr) {
-		
+		Collections.sort(arr);
+		Collections.reverse(arr);
+		System.out.println("ÅÅÐòºó½á¹û£º");
+		for(int i=0;i<arr.size();i++)
+		{
+			int temp = (int)arr.get(i);
+			System.out.print(temp+" ");
+		}
+		System.out.println("");
 	}
 
 }
